9f88176c47d5b49a48b9976b2c7c51746ccf73e3
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-12 21:37:15 -0600

    Controller lint temple by id query update

--- src/controllers/temple.js
+++ src/controllers/temple.js
@@ -1,13 +1,13 @@
-const db = require('../models');
+const db = require("../models");
 const Temple = db.temples;
 
 const apiKey =
-  'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N';
+  "Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N";
 
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.name) {
-    res.status(400).send({ message: 'Content can not be empty!' });
+    res.status(400).send({ message: "Content can not be empty!" });
     return;
   }
 
@@ -27,14 +27,14 @@ exports.create = (req, res) => {
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || 'Some error occurred while creating the Temple.',
+          err.message || "Some error occurred while creating the Temple.",
       });
     });
 };
 
 exports.findAll = (req, res) => {
-  console.log(req.header('apiKey'));
-  if (req.header('apiKey') === apiKey) {
+  console.log(req.header("apiKey"));
+  if (req.header("apiKey") === apiKey) {
     Temple.find(
       {},
       {
@@ -52,33 +52,43 @@ exports.findAll = (req, res) => {
       .catch((err) => {
         res.status(500).send({
           message:
-            err.message || 'Some error occurred while retrieving temples.',
+            err.message || "Some error occurred while retrieving temples.",
         });
       });
   } else {
-    res.send('Invalid apiKey, please read the documentation.');
+    res.send("Invalid apiKey, please read the documentation.");
   }
 };
 
 // Find a single Temple with an id
 exports.findOne = (req, res) => {
   const temple_id = req.params.temple_id;
-  if (req.header('apiKey') === apiKey) {
-    Temple.find({ temple_id: temple_id })
+  if (req.header("apiKey") === apiKey) {
+    Temple.find(
+      { temple_id: temple_id },
+      {
+        temple_id: 1,
+        name: 1,
+        location: 1,
+        dedicated: 1,
+        additionalInfo: 1,
+        _id: 0,
+      }
+    )
       .then((data) => {
         if (!data)
           res
             .status(404)
-            .send({ message: 'Not found Temple with id ' + temple_id });
+            .send({ message: "Not found Temple with id " + temple_id });
         else res.send(data[0]);
       })
       .catch((err) => {
         res.status(500).send({
-          message: 'Error retrieving Temple with temple_id=' + temple_id,
+          message: "Error retrieving Temple with temple_id=" + temple_id,
         });
       });
   } else {
-    res.send('Invalid apiKey, please read the documentation.');
+    res.send("Invalid apiKey, please read the documentation.");
   }
 };
 
